9f43b7aee62b5e91ad95c48fc1df820e4843d012
Pedro Eduardo <pedroordep5000@gmail.com>
2021-07-23 19:33:14 -0300

    Initial commit

---
+++ src/main/java/org/example/Funcionario.java
@@ -0,0 +1,213 @@
+package org.example;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+class Funcionarios{
+    Scanner teclado = new Scanner(System.in);
+
+    private String nome, sexo;//nascimento é a data de nascimento
+    Date nascimento;
+    private int idade;
+    private String dataRecebida;
+    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+    ArrayList<Funcionarios> bdFuncionarios = new ArrayList<Funcionarios>();
+
+
+    public Funcionarios(String nome, String sexo, Date nascimento, int idade, ArrayList<Funcionarios> bdFuncionarios) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nascimento = nascimento;
+        this.idade = idade;
+        this.bdFuncionarios = bdFuncionarios;
+    }
+
+    public Funcionarios() {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nascimento = nascimento;
+        this.idade = idade;
+        this.bdFuncionarios = bdFuncionarios;
+    }
+
+
+    public ArrayList<Funcionarios> getBdFuncionarios() {
+        return bdFuncionarios;
+    }
+
+    public void setBdFuncionarios(ArrayList<Funcionarios> bdFuncionarios) {
+
+        bdFuncionarios.add(new Funcionarios());
+
+        this.bdFuncionarios = bdFuncionarios;
+    }
+//    public void bdFuncionariosAdd(String nome, String sexo, Date nascimento, int idade {
+//        nome = getNome();
+//        sexo = getSexo();
+//        nascimento = getNascimento();
+//        idade = getIdade();
+//
+//        bdFuncionarios.add(new Funcionarios());
+//    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public int getIdade() {
+//        GregorianCalendar hj=new GregorianCalendar();
+//        GregorianCalendar nascimento=new GregorianCalendar();
+//        if(getNascimento()!=null){
+//            nascimento.setTime(getNascimento());
+//        }
+//        int anohj=hj.get(Calendar.YEAR);
+//        int anoNascimento=nascimento.get(Calendar.YEAR);
+//        return new Integer(anohj-anoNascimento);
+
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void cadastraFuncionario() throws ParseException {
+        String opcaoCadastro;
+
+        if (bdFuncionarios.size()<3){
+            for (int i=0; i<= bdFuncionarios.size(); i++){
+                System.out.println("Insira o nome do funcionario(a): ");
+                this.nome = teclado.nextLine();
+                setNome(nome);
+
+                System.out.println("Insira o sexo do(a) funcionario(a): ");
+                this.sexo = teclado.nextLine();
+                setSexo(sexo);
+
+                System.out.println("Inisra a dara de nascimento do(a) funcionario(a):  ");
+                dataRecebida = teclado.nextLine();
+
+                Date dataFormatada = formato.parse(dataRecebida);
+                setNascimento(dataFormatada);
+                GregorianCalendar hj=new GregorianCalendar();
+                GregorianCalendar nascimento=new GregorianCalendar();
+                if(getNascimento()!=null){
+                    nascimento.setTime(getNascimento());
+                }
+                int anohj=hj.get(Calendar.YEAR);
+                int anoNascimento=nascimento.get(Calendar.YEAR);
+                idade = new Integer(anohj-anoNascimento);
+                setIdade(idade);
+
+                setBdFuncionarios(bdFuncionarios);
+                System.out.println(i);
+                System.out.println(bdFuncionarios.size());
+
+                if (bdFuncionarios.size()>=3){
+                    System.out.println("Você deseja cadastrar mais funcionario?\nSim\nNão");
+                    opcaoCadastro = teclado.nextLine();
+                    if(opcaoCadastro.equals("Sim")){
+                        cadastraFuncionario();
+                    }
+                    else if(opcaoCadastro.equals("Não")){
+                        System.out.println("Foi um prazer auxilia-lo, qualquer coisa só executar novmamente");
+                        System.exit(0);
+                    }
+                    else{
+                        System.out.println("Opção unvalida. Selecione uma opção valida");
+                        cadastraFuncionario();
+                    }
+
+                }
+            }
+        }
+        else {
+            this.nome = teclado.nextLine();
+            setNome(nome);
+            teclado.nextLine();
+
+            System.out.println("Insira o sexo do(a) funcionario(a): ");
+            this.sexo = teclado.nextLine();
+            setSexo(sexo);
+            teclado.nextLine();
+
+            System.out.println("Inisra a dara de nascimento do(a) funcionario(a):  ");
+            dataRecebida = teclado.nextLine();
+            teclado.nextLine();
+
+            Date dataFormatada = formato.parse(dataRecebida);
+            setNascimento(dataFormatada);
+            GregorianCalendar hj=new GregorianCalendar();
+            GregorianCalendar nascimento=new GregorianCalendar();
+            if(getNascimento()!=null){
+                nascimento.setTime(getNascimento());
+            }
+            int anohj=hj.get(Calendar.YEAR);
+            int anoNascimento=nascimento.get(Calendar.YEAR);
+            idade = new Integer(anohj-anoNascimento);
+            setIdade(idade);
+
+            setBdFuncionarios(getBdFuncionarios());
+
+
+            System.out.println("Você deseja cadastrar mais funcionario?\nSim\nNão");
+            opcaoCadastro = teclado.nextLine();
+            if(opcaoCadastro.equals("Sim")){
+                cadastraFuncionario();
+            }
+            else if(opcaoCadastro.equals("Não")){
+                System.out.println("Foi um prazer auxilia-lo, qualquer coisa só executar novmamente");
+                System.exit(0);
+            }
+            else{
+                System.out.println("Opção unvalida. Selecione uma opção valida");
+                cadastraFuncionario();
+            }
+        }
+    }
+
+    public void emitirRelatorio() throws ParseException {
+
+        if(bdFuncionarios.size()<10){
+            System.out.println("Não teem 10 funcionarios cadastrados ainda, favor cadastrar.");
+            cadastraFuncionario();
+        }
+        else {
+            for (int i = 0; i <= bdFuncionarios.size(); i++) {
+                Iterator it = bdFuncionarios.iterator();
+
+                while (it.hasNext()) {
+                    System.out.println(it.next());
+                }
+            }
+        }
+    }
+    public void teste(){
+        Iterator it = bdFuncionarios.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class main
+{
+
+    public static void main( String[] args ) throws ParseException {
+        Scanner teclado = new Scanner(System.in);
+        Funcionarios funcionarios = new Funcionarios();
+        int opcao;
+        System.out.println( "Selecione uma opção:\n1 - Cadastrar funcionario\n2 - Emitir relatorio" );
+        opcao = teclado.nextInt();
+
+        if(opcao == 1){
+
+            funcionarios.cadastraFuncionario();
+
+        }
+        else if (opcao == 2){
+            funcionarios.emitirRelatorio();
+        }
+        else if(opcao ==3){
+
+        }
+        else{
+            System.out.println("Opção invalida, selecione uma opção.");
+        }
+    }
+
+
+}
